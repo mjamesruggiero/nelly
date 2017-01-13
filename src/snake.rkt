d40834f03fbd5b531273dae7b02ab75a3714c651
mjamesruggiero <mjamesruggiero@gmail.com>
2017-01-12 20:41:49 -0800

    WIP

--- src/snake.rkt
+++ src/snake.rkt
@@ -120,3 +120,101 @@
   (cond [(empty? (rest segs)) empty]
         [else (cons (first segs)
                     (all-but-last (rest segs)))]))
+
+;; renew and rot goos
+(define (age-goo goos)
+  (rot (renew goos)))
+
+;; renew rotten goos
+(define (renew goos)
+  (cond [(empty? goos) empty]
+        [(rotten? (first goos))
+         (cons (fresh-goo) (renew (rest goos)))]
+        [else
+         (cons (first goos) (renew (rest goos)))]))
+
+;; rot all the goos
+(define (rot goos)
+  (cond [(empty? goos) empty]
+        [else (cons (decay (first goos))
+                    (rot (rest goos)))]))
+
+;; has the goo expired?
+(define (rotten? g)
+  (zero? (goo-expire g)))
+
+;; decreases the expire field of goo by one
+(define (decay g)
+  (goo (goo-loc g) (sub1 (goo-expire g))))
+
+;; create random goo with fresh expiration
+(define (fresh-goo)
+  (goo (posn (add1 (random (sub1 SIZE)))
+             (add1 (random (sub1 SIZE))))
+       EXPIRATION-TIME))
+
+;;~~~~~~~~~~~~~~~~~~~~~~~~~
+;;      keys
+;;~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(define dir? x
+  (or (string=? x "up")
+      (string=? x "down")
+      (string=? x "left")
+      (string=? x "right")))
+
+;; change the direction (if not opposite current direction)
+(define (world-change-dir w d)
+  (define the-snake (pit-snake w))
+  (cond [(and (opposite-dir? (snake-dir the-snake) d)
+              ;; consists of the head and at least one segment
+              (cons? (rest (snake-segs the-snake))))
+         (stop-with w)]
+        [else (pit (snake-change-dir the-snake d)
+                   (pit-goos w))]))
+
+;; are d1 and d2 opposites?
+(define (opposite-dir? d1 d2)
+  (cond [(string=? dl "up")    (string=? "down")]
+        [(string=? dl "down")  (string=? "up")]
+        [(string=? dl "left")  (string=? "right")]
+        [(string=? dl "right") (string=? "left")]))
+
+
+;;~~~~~~~~~~~~~~~~~~~~~~~~~
+;;      render
+;;~~~~~~~~~~~~~~~~~~~~~~~~~
+
+;; draws the snake onto the scene
+(define (snake+scene snake scene)
+  (define snake-body-scene
+    (img-list+scene (snake-body snake) SEG-IMG scene))
+  (define dir (snake-dir snake))
+  (img+scene (snake-head snake)
+             (cond [(string=? "up" dir) HEAD-UP-IMG]
+                   [(string=? "down" dir) HEAD-DOWN-IMG]
+                   [(string=? "left" dir) HEAD-LEFT-IMG]
+                   [(string=? "right" dir) HEAD-RIGHT-IMG])
+             snake-body-scene))
+
+;; draws all the goo to a scene
+(define (goo-list+scene goos scene)
+  (define (get-posns-from-goo goos)
+    (cond [(empty? goos) empty]
+          [else (cons (goo-loc (first goos))
+                      (get-posns-from-goo (rest goos)))]))
+  (img-list+scene (get-posns-from-goo goos) GOO-IMG scene))
+
+;; draws the image to each position in the list
+(define (img-list+scene posns img scene)
+  (cond [(empty? posns) scene]
+        [else (img+scene (first posns)
+                         img
+                         (img-list+scene (rest posns) img scene))]))
+
+;; draws the given image onto the scene at position
+(define (img+scene posn img scene)
+  (place-image img
+               (* (posn-x posn) SEG-SIZE)
+               (* (posn-y posn) SEG-SIZE)
+               scene))
